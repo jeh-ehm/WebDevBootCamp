09ffde1572f669564180a129816ce02655a5f827
JanMarc <74803208+jeh-ehm@users.noreply.github.com>
2021-06-29 16:45:18 +0800

    halfway through simon game

--- jQuery/Simon Game Challenge Starting Files/game.js
+++ jQuery/Simon Game Challenge Starting Files/game.js
@@ -1,10 +1,36 @@
 
+var gamePattern = [];
+var userClickedPattern = [];
 var buttonColours = ["red", "blue", "green", "yellow"];
-var randomChosenColour = buttonColours[nextSequence()];
+
+$(document).keydown(nextSequence);
+
+$(".btn").on("click", function () {
+	var userChosenColour = $(this).attr("id");
+	userClickedPattern.push(userChosenColour);
+	playSound(userChosenColour);
+	animatePress(userChosenColour);
+});
 
 function nextSequence() {
+
 	var randomNumber = Math.floor(Math.random() * 4) ;
-	return randomNumber;
+	var randomChosenColour = buttonColours[randomNumber];
+
+	gamePattern.push(randomChosenColour);
+	$("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+	playSound(randomChosenColour);
 }
 
-console.log(randomChosenColour);
+function playSound(name) {
+	var audio = new Audio("sounds/" + name + ".mp3");
+	audio.play();
+}
+
+function animatePress(currentColour) {
+	$("." + currentColour).addClass("pressed");
+	setTimeout(() => {
+		$("." + currentColour).removeClass("pressed");
+	}, 100);
+	
+}
